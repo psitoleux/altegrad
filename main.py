660ff41955dc05fdee29d8cce94ee2889b31588f
Paul SITOLEUX <paul@MBP-de-Paul.home>
2024-01-11 22:33:15 +0100

    typo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dataloader impo rt GraphTextDataset, GraphDataset, TextDataset
+from dataloader import GraphTextDataset, GraphDataset, TextDataset
 from torch_geometric.data import DataLoader
 from torch.utils.data import DataLoader as TorchDataLoader
 from Model import Model
