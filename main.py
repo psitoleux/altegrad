2e11d5524c6fea31a64f55eb086b4b1117a7c9fe
psitoleux <paul.sitoleux+github@me.com>
2024-01-12 19:51:33 +0100

    torch import error

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def contrastive_loss(v1, v2):
   labels = torch.arange(logits.shape[0], device=v1.device)
   return CE(logits, labels) + CE(torch.transpose(logits, 0, 1), labels)
 
-BCEL = nn.BCEWithLogitsLoss()
+BCEL = torch.nn.BCEWithLogitsLoss()
 
 def negative_sampling_contrastive_loss(v1, v2, labels):
   logits = torch.matmul(v1,torch.transpose(v2, 0, 1))
