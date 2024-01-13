84994327253a3820c49f12d9af09f46397180194
psitoleux <paul.sitoleux+github@me.com>
2024-01-13 23:55:12 +0100

    variable mistake

--- dataloader_2.py
+++ dataloader_2.py
@@ -33,7 +33,7 @@ class LabelDataset(Dataset):
         self.labels = df_new[2]
         df_new = df_new.drop(2, axis = 'columns')
 
-        self.description = self.description.set_index(0).to_dict()
+        self.description = df_new.set_index(0).to_dict()
         self.cids = list(self.description[1].keys())
         
         self.idx_to_cid = {}
