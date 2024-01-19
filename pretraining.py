9c3dad0f00d01f4791233a86b8cc94841ab74f7b
psitoleux <paul.sitoleux+github@me.com>
2024-01-19 16:11:27 +0100

    typo

--- pretraining.py
+++ pretraining.py
@@ -37,7 +37,7 @@ nout = 768
 num_node_features, nhid, graph_hidden_channels = 300, 300, 300
 graph_encoder = GATEncoder(num_node_features, nout, nhid, graph_hidden_channels).to(device)
 
-optimizer_pt = optim.AdamW(graph_encoder.parameters(), lr=lr_pt,
+optimizer_pt = optim.AdamW(graph_encoder.parameters(), lr=lr,
                             betas=(0.9, 0.999),
                             weight_decay=0.01)
 
