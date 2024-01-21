25b26662cf0a9bc3e953eff3a1ab2f048e67406e
psitoleux <paul.sitoleux+github@me.com>
2024-01-21 18:22:25 +0100

    typo

--- Model.py
+++ Model.py
@@ -92,7 +92,6 @@ class TextEncoder(nn.Module):
     def __init__(self, model_name):
         super(TextEncoder, self).__init__()
         self.bert = AutoModel.from_pretrained(model_name)
-        self.
         
     def forward(self, input_ids, attention_mask): 
         encoded_text = self.bert(input_ids, attention_mask=attention_mask)
